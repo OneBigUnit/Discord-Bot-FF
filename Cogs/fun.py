7c7d6ba6c629b10df2a8f27e615d6aab031a86e1
OneBigUnit <08klock@brightoncollege.net>
2021-05-07 12:28:06 +0100

    Add Kahoot functionality, and update requirements.txt

--- Cogs/fun.py
+++ Cogs/fun.py
@@ -3,6 +3,7 @@ import random
 import discord
 from discord.ext import commands
 
+from Utilities.kahoot import KahootBot
 from main import database
 
 
@@ -63,6 +64,24 @@ class Fun(commands.Cog):
         else:
             data = dict(data)
             await ctx.send(f"{user.mention}'s first penis score was: {data['First Penis Score']}")
+
+    @commands.command(aliases=["kahootbot", "kbot"])
+    async def kahoot(self, ctx, *args):
+        _vars = [None, None, None, 0.0, 0.0, False]
+        for idx, (arg, var) in enumerate(zip(args, _vars)):
+            _vars[idx] = arg
+        if None in _vars[:3]:
+            raise commands.errors.MissingRequiredArgument() from None
+        username, game_pin, game_id, max_delay, min_delay, debug = _vars
+        kahoot_bot = KahootBot(debug=debug, minimum_answer_delay=min_delay, maximum_answer_delay=max_delay)
+        if debug:
+            warning_msg = await ctx.send(
+                "Client Message: [Warning] --> If the bot fails, please note: This bot only works with public "
+                "kahoots, and it can only answer True/False and Quiz Type questions.")
+            await warning_msg.delete(delay=5)
+            async for message in kahoot_bot.play_game(username, game_pin, game_id):
+                bot_msg = await ctx.send(message)
+                await bot_msg.delete(delay=5)
 
 
 def setup(bot):
